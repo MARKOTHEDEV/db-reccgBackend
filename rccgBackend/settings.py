e3c7f4cd8beab768778c3119652b2c99f6ae557a
MARKOTHEDEV <ogechuwkumatthew@gmail.com>
2021-12-02 08:51:01 -0600

    added "arccgbackend.herokuapp.com" to allowed host

--- rccgBackend/settings.py
+++ rccgBackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['djang_secret_key']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','rccgbackend.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','rccgbackend.herokuapp.com',]
 
 
 # Application definition
